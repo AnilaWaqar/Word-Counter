a4b1782f75d4a69e1ef912bca08fb8ae46c2ecbf
AnilaWaqar <160339061+AnilaWaqar@users.noreply.github.com>
2024-04-21 19:14:37 -0700

    Words Counter

---
+++ app.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgCyanBright("\n                                    "));
+console.log(chalk.bgCyanBright("         " +
+    (" WORDS ") + "  " + (" COUNTER ") + "         "));
+console.log(chalk.bgCyanBright("                                    \n"));
+console.log(chalk.bgCyanBright("   " + chalk.bgBlue.bold(" We Will Count the Words of your Given Text ") + "   "));
+let condition = true;
+while (condition) {
+    const answer = await inquirer.prompt([
+        {
+            name: "Scentence",
+            message: chalk.bgCyan.bold("\nEnter Your Sentences for countig the words: "),
+            type: "input"
+        }
+    ]);
+    const words = answer.Scentence.trim().split(" ");
+    console.log("\n");
+    console.log(words);
+    console.log(chalk.bgCyan.bold("\nYour Words: ") + chalk.bgBlue.bold(words));
+    console.log(chalk.bold("\nTotal Words in your Given Text is:"), chalk.bgBlue.bold(" " + words.length + " "));
+    const forMore = await inquirer.prompt([
+        {
+            name: "wantMore",
+            message: chalk.bold("\nDo you want to Count more Words?"),
+            type: "confirm",
+            default: true
+        }
+    ]);
+    condition = forMore.wantMore;
+}
+console.log(chalk.bgCyanBright("\n                                 "));
+console.log(chalk.bgCyanBright("       " + chalk.bold(" Thanks For Using Word Counter") + "        "));
+console.log(chalk.bgCyanBright("                                 "));
